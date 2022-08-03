4b261446cc1a24adc7ac401117db36283f72bc81
Taco Droogers <taco@pubble.nl>
2022-08-03 10:02:02 +0200

    Add Class to Chart.razor

--- ChartjsDemo/Data/LineDataExamples.cs
+++ ChartjsDemo/Data/LineDataExamples.cs
@@ -2,7 +2,7 @@
 {
     public static class LineDataExamples
     {
-        public static List<string> SimpleLineText = new List<string>() { "January", "February", "March", "Aprl", "May", "June", "July" };
+        public static List<string> SimpleLineText = new List<string>() { "January", "February", "March", "April", "May", "June", "July" };
         public static List<decimal> SimpleLine = new List<decimal>() { 65, 59, 80, 81, 56, 55, 40 };
         public static List<decimal> SimpleLine2 = new List<decimal>() { 33, 25, 35, 51, 54, 76, 45 };
     }
