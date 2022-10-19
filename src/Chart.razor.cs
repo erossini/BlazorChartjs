9d2ce980b3006e4e40e28a554235f51d4e566235
macias <bantu@poczta.fm>
2022-10-19 17:38:14 +0200

    onmouseout

--- src/Chart.razor.cs
+++ src/Chart.razor.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace PSC.Blazor.Components.Chartjs
 {
@@ -105,6 +106,14 @@ namespace PSC.Blazor.Components.Chartjs
             this.oldReference?.Dispose();
         }
 
+        private ValueTask OnMouseOut(MouseEventArgs mouseEventArgs)
+        {
+            if (Config.Options is Options {OnMouseOutAsync: { }} options)
+                return options.OnMouseOutAsync(mouseEventArgs);
+            else
+                return ValueTask.CompletedTask;
+        }
+        
         #region JavaScript invokable functions
 
         [JSInvokable]
